4ce9e7bd4ddfff2b1ba2e450b8c41c438edd6b74
supermartynov <suppermartynov@gmail.com>
2021-06-21 20:14:26 +0300

    загрузили данные на пользователя

--- front/src/components/CurrentSpecialty.vue
+++ front/src/components/CurrentSpecialty.vue
@@ -1,19 +1,37 @@
 <template>
-<div>
-hello {{data}}
-</div>
+<v-app>
+<div class="img"></div>
+</v-app>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
 name: "CurrentSpecialty",
 data() {
   return {
-    data: 'высв'
+    specialtyId: '',
+    vacancies: {},
+    specialtyParams: {},
+    skillIds: {},
+    imgUrl: {}
   }
 },
 created() {
-  this.data = this.$route.params.id
+  this.specialtyId = this.$route.params.id;
+  axios.get(`http://localhost:5005/vacancies?specialty_id=${this.specialtyId}`)
+      .then(res => this.vacancies = res.data);//[ "id", "company_name", "min_salary", "max_salary", "description", "location", "url", "specialty_id"]
+  axios.get(`http://localhost:5005/specialties/${this.specialtyId}`)
+    .then(res => this.specialtyParams = res.data)
+  axios.get(`http://localhost:5005/skillSpecialty?specialty_id=${this.specialtyId}`)
+      .then(res => this.skillIds = res.data)
+},
+mounted() {
+
+},
+methods: {
+
 }
 }
 </script>
