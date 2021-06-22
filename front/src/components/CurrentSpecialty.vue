fe8e00ff12ad5cd956fc5bbf68d2f2326d73753a
supermartynov <suppermartynov@gmail.com>
2021-06-22 12:41:04 +0300

    Almost ready, prepairing for vacancies card

--- front/src/components/CurrentSpecialty.vue
+++ front/src/components/CurrentSpecialty.vue
@@ -9,18 +9,48 @@
       ></v-img>
       <div class="centered">{{ specialtyName }}</div>
     </div>
-    <div class="container">
-      <div class="row">
-        <div class="content">
-          <div class="description">
-            <h2 class="headder">Чем занимается специалист?</h2>
-            {{ description }}
+    <div>
+      <div class="sidebar"></div>
+      <div class="content">
+        <div class="description">
+          <h3 class="headder"> Краткое описание </h3>
+          {{description}}
+          <br>
+        </div>
+        <div class="skills">
+          <h3 class="headder">Необходимые навыки для успешной работы</h3>
+          <div class="skillsInfo">
+            <h4 class="subheadder">Важные личные качества: </h4>
+            <ul>
+              <li v-for="key in softSkills" :key="key">
+                {{ key }}
+              </li>
+            </ul>
+          </div>
+          <div class="skillsInfo">
+            <h4 class="subheadder">Важные профессиональные качества: </h4>
+            <ul>
+              <li v-for="key in hardSkills" :key="key">
+                {{ key }}
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="sidebar"> {{ description }}</div>
+        <div class="salary">
+          <h4 class="subheadder">Средняя зарплата в Великобритании:</h4>
+          <div class="wrappLeft">{{ salary }} £ в год</div>
+        </div>
+        <div class="vacancies">
+          <h3 class="headder">Вакансии в Великобритании: </h3>
+          <div class="list" v-for="(vacancy, index) in vacancies" :key="index">
+            <vacancy-card :id="specialty.id" :specialtyName="specialty.name"></vacancy-card>
+          </div>
+        </div>
       </div>
+      <div class="sidebar"></div>
     </div>
-    <div class="footer">{{ description }}</div>
+    <div class="footer">
+    </div>
 
 
   </v-app>
@@ -28,33 +58,64 @@
 
 <script>
 import axios from "axios";
+import VacancyCard from "@/components/vacancyCard";
 
 export default {
   name: "CurrentSpecialty",
+  components: {
+    VacancyCard,
+  },
   data() {
     return {
       specialtyId: '',
       vacancies: {},
       specialtyParams: {},
-      skillIds: {},
+      skillIds: [],
       imgUrl: '',
       specialtyName: '',
-      description: ''
+      description: '',
+      hardSkills: [],
+      softSkills: [],
+      salary: '',
+      company_name: '',
+      min_salary: '',
+      max_salary: '',
+      location: '',
+      vacancyDescription: '',
+      vacancyUrl: '',
     }
   },
   created() {
     this.specialtyId = this.$route.params.id;
     axios.get(`http://localhost:5005/vacancies?specialty_id=${this.specialtyId}`)
-        .then(res => this.vacancies = res.data);//[ "id", "company_name", "min_salary", "max_salary", "description", "location", "url", "specialty_id"]
+        .then(res => {
+          console.log(res)
+          /*this.company_name = res.data["company_name"]
+          this.min_salary = res.data["min_salary"]
+          this.max_salary = res.data["max_salary"]
+          this.vacancyDescription = res.data["vacancyDescription"]
+          this.vacancyUrl = res.data["vacancyUrl"]*/
+        });//[ "id", "company_name", "min_salary", "max_salary", "description", "location", "url", "specialty_id"]
     axios.get(`http://localhost:5005/specialties/${this.specialtyId}`)
         .then(res => {
           this.imgUrl = res.data["imgUrl"];
           this.specialtyName = res.data["name"];
           this.description = res.data["description"]
+          this.salary = Math.round(res.data["overageSalary"])
         })
     axios.get(`http://localhost:5005/skillSpecialty?specialty_id=${this.specialtyId}`)
-        .then(res => this.skillIds = res.data)
-
+        .then(res => {
+          for (const key of res.data){
+            axios.get(`http://localhost:5005/skills/${key["skill_id"]}`)
+            .then(res => {
+              if (res["data"]["type"] === 1) {
+                this.softSkills.push(res["data"]["skill_name"])
+              } else if(res["data"]["type"] === 2) {
+                this.hardSkills.push(res["data"]["skill_name"])
+              }
+            })
+          }
+        })
   },
   mounted() {
   },
@@ -70,13 +131,13 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 .img img {
   width: 100%;
   height: 500px;
 }
 
 .containerImg {
-  margin-bottom: 10px;
   position: relative;
   text-align: center;
   color: white;
@@ -92,37 +153,69 @@ export default {
 }
 
 .description {
-  font-family: Bookman, serif;
-  font-size: 14px;
-  text-align: center;
+  text-align: left;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  margin-left: 10px;
+  margin-right: 10px;
 
 }
 
-.description {
+.content {
+  width: 80%;
+  float: left;
+  min-height: 400px;
+  text-align: left;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.sidebar {
+  width: 10%;
+  float: left;
+  height: 100%;
+  background: #e3e3e3 ;
+}
+
+.headder {
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.subheadder {
+  text-align: left;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  margin-left: 7px;
+}
+
+.footer {
+  min-height: 20px;
+  margin-top: 10px;
+  background: #808080;
+  height: 100%;
+  width: 100%;
+  text-align: left;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   margin: auto;
+}
+
+.skillsInfo {
   text-align: left;
-  width: 70%;
 }
 
-.content {
-  width: 70%;
-  float: left;
-  min-height: 500px;
+.wrappLeft {
+  margin-left: 15px;
+  display: block;
 }
 
-.sidebar {
-  width: 30%;
-  float: left;
-  background: red;
-  min-height: 200px;
-}
-
-.headder {
-  text-align: center;
-}
 
 </style>
---
+++ front/src/components/vacancyCard.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+name: "vacancyCard"
+}
+</script>
+
+<style scoped>
+
+</style>
