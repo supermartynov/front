16d56cce46b2fb2b69624ab9ec8e9b1d87ebb483
supermartynov <suppermartynov@gmail.com>
2021-06-22 15:20:00 +0300

    не работают карточки

--- front/src/components/vacancyCard.vue
+++ front/src/components/vacancyCard.vue
@@ -1,18 +1,20 @@
 <template>
 <v-app>
-  <v-card class="mx-auto" max-width="400">
-    <v-card-text>
-      <p>{{ specialty_name  }}</p>
-      <div class="companyName"><p>Название компании :  {{company_name}}</p></div>
-      <p>Зарплата :  {{Math.round(max_salary)}} £ в год</p>
-      <p>Местонахождение: {{ location }}</p>
-    </v-card-text>
-    <v-card-actions>
-      <a :href="vacancyUrl" target="_blank">
-        <v-btn text color="black accent-4">Подробнее</v-btn>
-      </a>
-    </v-card-actions>
-  </v-card>
+      <v-card class="mx-auto" max-width="400">
+        <v-card-text>
+          <p>{{ specialty_name  }}</p>
+          <div class="companyName"><p>Название компании :  {{company_name}}</p></div>
+          <p>Зарплата :  {{Math.round(max_salary)}} £ в год</p>
+          <p>Местонахождение: {{ location }}</p>
+        </v-card-text>
+        <v-card-actions>
+          <a :href="vacancyUrl" target="_blank">
+            <v-btn text color="black accent-4">Подробнее</v-btn>
+          </a>
+        </v-card-actions>
+      </v-card>
+
+
 </v-app>
 </template>
 
@@ -30,11 +32,17 @@ props: ['min_salary', 'max_salary', 'location', 'vacancyDescription', 'vacancyUr
 .companyName {
   font-size: 14px;
 }
-
+div.list {
+  width: 300px;
+  margin-top: 20px;
+  justify-content: flex-start;
+}
 .card {
   margin-top: -200px;
   width: 300px;
   padding: 10px 20px 10px 20px;
 }
+
+
 
 </style>
